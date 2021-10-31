285da84a5645c6fc89c480b76525cb5adbf766fc
Brent Acs <brent.r.acs@gmail.com>
2021-10-31 16:38:27 -0400

    Fleshed out initial hosted console app

---
+++ Templates/Projects/ConsoleHosted/MainService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleHosted
+{
+  internal class MainService : IHostedService
+  {
+    private readonly IConfiguration _configuration;
+    private readonly IHostApplicationLifetime _appLifetime;
+    private readonly ILogger _logger;
+
+    public MainService(IConfiguration configuration,
+      IHostApplicationLifetime appLifetime,
+      ILogger<MainService> logger)
+    {
+      _configuration = configuration;
+      _appLifetime = appLifetime;
+      _logger = logger;
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+      _logger.LogInformation($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+      
+      _appLifetime.ApplicationStarted.Register(() =>
+      {
+        Task.Run(async () =>
+        {
+          try
+          {
+            await ServiceMain().ConfigureAwait(false);
+            _logger.LogInformation("done.");
+          }
+          catch (Exception ex)
+          {
+            _logger.LogError(ex, $"Unhandled exception: '{ex.Message}'");
+          }
+          finally
+          {
+            _appLifetime.StopApplication();
+          }
+        }, cancellationToken);
+      });
+
+      return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      return Task.CompletedTask;
+    }
+
+    private Task ServiceMain()
+    {
+       Console.WriteLine("Hello, World: the Hosted Service.");
+
+       return Task.CompletedTask;
+    }
+  }
+}
--- Templates/Projects/ConsoleHosted/Program.cs
+++ Templates/Projects/ConsoleHosted/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace ConsoleHosted
 {
   internal class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-      Console.WriteLine("Hello World, via Templates!");
+      IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables()
+        .Build();
+      Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+
+      await Host.CreateDefaultBuilder(args)
+        .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+        .ConfigureLogging(logging =>
+        {
+        })
+        .UseSerilog()
+        .ConfigureServices((hostContext, services) =>
+        {
+          services.AddHostedService<MainService>();
+        })
+        .RunConsoleAsync();
     }
   }
 }
